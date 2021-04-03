8ab01ef16e339d0e76bbe82fec06aec5c1303bfa
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2021-04-03 17:50:49 +0200

    Settings for Wifi and Pet name

---
+++ tamaguino/settings.h
@@ -0,0 +1,10 @@
+/**
+ * Settings for EiEiEi - Cloud connected
+ * 
+ * WIFI_*: Access to yout wifi network
+ * PET_NAME: Name of your pet to be shown in event log
+ */
+
+#define WIFI_SSID "WifiName"
+#define WIFI_PASS "SuperG3he1M"
+#define PET_NAME "Dino"
